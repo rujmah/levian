487e0e776c34f1928c4d4ccf27380c4e9ee13bc3
rujmah <rjjm@rjjm.net>
2013-05-31 22:00:47 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Levian::Application.config.secret_token = 'e96fc13a4bee172af4b6927cb89754be4d95fe4061c4adc8dd48f27518553979c904d947cf48a390c9fcd48eb8cda180bb2e86f8b71a1392b05f6145c9448335'
